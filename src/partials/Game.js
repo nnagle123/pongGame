d70f295041a862b13a06c2dc37d83758b3232e7a
unknown <nathannagle1@gmail.com>
2019-04-25 13:56:14 -0400

    Paddles moving.

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,70 @@
+import {SVG_NS, KEYS} from '../settings';
+import Board from './Board';
+import Paddle from './Paddle';
+ 
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
 
-		// Other code goes here...
-  }
+    this.gameElement = document.getElementById(this.element)
 
+    //Create instance of board
+    this.board = new Board(this.width, this.height);
+ 
+
+  //Instance for Player Paddles
+
+  this.paddleWidth = 8;
+  this.paddleHeight = 56;
+  this.boardGap = 10;
+
+  //Player 1 instance
+
+  this.player1 = new Paddle(
+      this.height,
+      this.paddleWidth,
+      this.paddleHeight,
+      this.boardGap,
+      ((this.height - this.paddleHeight)/2),
+      KEYS.a,
+      KEYS.b,
+
+
+  )
+
+// Player 2 instance
+
+  this.player2 = new Paddle(
+    this.height,
+    this.paddleWidth,
+    this.paddleHeight,
+    ((this.width - this.boardGap)-this.paddleWidth),
+    ((this.height - this.paddleHeight)/2),
+    KEYS.up,
+    KEYS.down,
+  )
+
+ }
   render() {
-		// More code goes here....
+
+    this.gameElement.innerHTML = '';
+
+let svg = document.createElementNS(SVG_NS, 'svg');
+
+svg.setAttributeNS(null, 'width', this.width);
+svg.setAttributeNS(null, 'height', this.height);
+svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+this.gameElement.appendChild(svg);
+
+
+//Renders board by passing svg canvas we created
+this.board.render(svg)
+
+//Render P1 and P2 paddles
+
+      this.player1.render(svg);
+      this.player2.render(svg);
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
     watchContentBase: true,
     historyApiFallback: true,
     open: true,
-    port: 3000,
+    port: 3001,
     publicPath: '/build/',
     stats: 'errors-only',
     watchOptions: {
